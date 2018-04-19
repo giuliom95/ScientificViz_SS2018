1a83d73509772712599274fcc0575934af934409
giuliom95 <giuliomartella1995@gmail.com>
2018-04-19 18:32:22 +0200

    Style improvement

--- hw1/es1-3.py
+++ hw1/es1-3.py
@@ -4,33 +4,28 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 if __name__=='__main__':
-    X = []
-    Y = []
-    Z = []
-
+    data = []
     max_tuple = (0,0,0)
 
     fd = open('Data', 'r')
     for line in fd.readlines():
         elems = line.split()
         if len(elems) == 3:
-            x = float(elems[0])
-            y = float(elems[1])
-            z = float(elems[2])
-            
-            if z > max_tuple[2]:
-                max_tuple = (x,y,z)
-                
-            X.append(x)
-            Y.append(y)
-            Z.append(z)
+            data.append([float(elems[0]), float(elems[1]), float(elems[2])])
 
-    minZ = min(Z)
-    maxZ = max(Z)
-
-    X = np.reshape(X, (24, 24))
-    Y = np.reshape(Y, (24, 24))
-    Z = np.reshape(Z, (24, 24))
+    data = np.array(data)
+    
+    maxZ_tuple = data[data.argmax(0)[2]]
+    
+    xs = data[:,0]
+    ys = data[:,1]
+    zs = data[:,2]
+    X = np.reshape(xs, (24,24))
+    Y = np.reshape(ys, (24,24))
+    Z = np.reshape(zs, (24,24))
+    
+    minZ = Z.min()
+    maxZ = maxZ_tuple[2]
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -40,12 +35,13 @@ if __name__=='__main__':
     ax.set_zlabel('z')
 
     ax.contourf(X,Y,Z, levels=np.linspace(minZ,maxZ,300))
+    
+    x,y,z = maxZ_tuple
+    ax.text(x, y, z+0.05, ' Max (x={}, y={}, z={})'.format(x,y,z))
+    ax.scatter([x],[y],[z], c='red')
+    ax.plot([x, x],[y, y],[z-.1, z+.1], c='red')
 
-    x,y,z = max_tuple
-    ax.text(x, y, z+.1, 'Max (x={}, y={}, z={})'.format(x,y,z))
-    ax.scatter([x],[y],[z+.01], c='red')
-
-
+    ax.view_init(20, 30)
     plt.show()
 
 
