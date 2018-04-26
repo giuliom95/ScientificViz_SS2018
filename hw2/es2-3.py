0e3545363d51ce31cf760a25f9f3d1118783a63c
giuliom95 <giuliomartella1995@gmail.com>
2018-04-26 13:01:31 +0200

    Boxplots on hw2

--- hw2/es2-3.py
+++ hw2/es2-3.py
@@ -4,18 +4,26 @@ from numpy.linalg import inv
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from functools import reduce
 
 def mdot(*args):
     return reduce(np.dot, args)
 
 
 if __name__=='__main__':
-    fig = plt.figure()
+    fig1 = plt.figure()
+    fig2 = plt.figure()
+    
+    xs = []
+    ys = []
     
     for i in range(1,5):
         in_data = np.loadtxt('./provided_files/Data{}.csv'.format(i), comments='x')
         x = in_data[:, 0]
         y = in_data[:, 1]
+        
+        xs.append(x)
+        ys.append(y)
         
         mean_x = np.mean(x)
         mean_y = np.mean(y)
@@ -24,7 +32,6 @@ if __name__=='__main__':
         var_y = np.var(x)
         
         corr = (sum((x-mean_x)*(y-mean_y)))/np.sqrt(sum((x-mean_x)**2)*sum((y-mean_y)**2))
-        
         print(corr)
         
         x_cap = np.column_stack([np.ones(len(x)), x])
@@ -33,16 +40,27 @@ if __name__=='__main__':
         line_x = np.array([0,20])
         line_y = beta[0] + line_x*beta[1]
         
-        
-        ax = fig.add_subplot(220 + i)
-        ax.set_xlabel('x_{}'.format(i))
-        ax.set_xlim(0, 20)
-        ax.set_ylabel('y_{}'.format(i))
-        ax.set_ylim(0, 13)
-        ax.scatter(x, y)
-        ax.scatter(mean_x, mean_y, c='red')
-        ax.scatter(var_x, var_y, c='green')
-        ax.plot(line_x, line_y, c='orange')
+
+        ax1 = fig1.add_subplot(220 + i)
+        ax1.set_xlabel('x_{}'.format(i))
+        ax1.set_xlim(0, 20)
+        ax1.set_ylabel('y_{}'.format(i))
+        ax1.set_ylim(0, 13)
+        ax1.scatter(x, y)
+        ax1.scatter(mean_x, mean_y, c='red')
+        ax1.scatter(var_x, var_y, c='green')
+        ax1.plot(line_x, line_y, c='orange')
+
+
+    ax2 = fig2.add_subplot(211)
+    ax2.set_ylabel('x')
+    ax2.set_xlabel('Datasets')
+    ax2.boxplot(xs)
+    
+    ax2 = fig2.add_subplot(212)
+    ax2.set_ylabel('y')
+    ax2.set_xlabel('Datasets')
+    ax2.boxplot(ys)
     
     plt.show()
     
